1199943c27198286bbf4ff2ca2b90645ce171480
tonyhuang2222 <1487651Ab@gmail.com>
2024-07-05 15:19:42 +0800

    Add files via upload

---
+++ src/components/Sections.vue
@@ -0,0 +1,49 @@
+<script setup>
+</script>
+
+<template>
+  <div class="">
+    <div class="row q-mt-xs-md q-mt-md-lg full-width items-center" style="background-color: #E0FFFF">
+      <q-img
+        src="~assets/school.png"
+        fit="contain"
+        class="col-xs-12 col-md-5"
+      ></q-img>
+      <div class="text-center col-xs-12 col-md-7 q-px-xs-md q-px-md-xl column q-mt-xs-md">
+        <h4 class="text-weight-bolder text-primary q-pb-lg feeling-passionate">My School</h4>
+        <h6 class="q-pb-xs">My school is more than just a place of learning; it's a community where I feel supported, challenged, and inspired to grow. I truly enjoy being here.</h6>
+      </div>
+    </div>
+
+
+    <div class="row reverse q-mt-xs-md q-mt-md-lg full-width items-center" style="background-color: #FFE5E0">
+      <q-img
+        src="~assets/friends.png"
+        fit="contain"
+        class="col-xs-12 col-md-5"
+      ></q-img>
+      <div class="text-center col-xs-12 col-md-7 q-px-xs-md q-px-md-xl column q-mt-xs-md">
+        <h4 class="text-weight-bolder text-primary q-pb-lg feeling-passionate">My Best Friends</h4>
+        <h6 class="q-pb-xs">My best friends are like family, always there for me through thick and thin. Their unwavering support and laughter make life brighter.</h6>
+      </div>
+    </div>
+
+
+    <div class="row q-mt-xs-md q-mt-md-lg full-width items-center" style="background-color: #E6E0FF">
+      <q-img
+        src="~assets/jojo.png"
+        fit="contain"
+        class="col-xs-12 col-md-5"
+      ></q-img>
+      <div class="text-center col-xs-12 col-md-7 q-px-xs-md q-px-md-xl column q-mt-xs-md">
+        <h4 class="text-weight-bolder text-primary q-pb-lg feeling-passionate">My Favourite charactar</h4>
+        <h6 class="q-pb-xs">The character Jojo holds a special place in my heart, making them my absolute favorite.</h6>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
